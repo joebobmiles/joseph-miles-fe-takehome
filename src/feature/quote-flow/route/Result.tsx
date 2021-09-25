501c14cb9e0bdfa39ef7c5e34ecdcdf219df94cb
Joseph R Miles <joe@jrm.dev>
2021-09-24 21:09:10 -0700

    feat: result page queries backend

--- src/feature/quote-flow/route/Result.tsx
+++ src/feature/quote-flow/route/Result.tsx
@@ -1,5 +1,99 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
-export const Result = (): JSX.Element => (
-  <></>
-)
+import { PARTNER_TOKEN } from '@/config'
+import { RootState } from '@/type'
+
+export const Result = (): JSX.Element => {
+  const [isLoading, setIsLoading] = React.useState(true)
+  const [response, setResponse] = React.useState<any>({})
+
+  const {
+    businessName,
+    contactEmail,
+    grossAnnualSales,
+    annualPayroll,
+    numEmployees,
+    industryId,
+    zip
+  } = useSelector<RootState, {
+    businessName: string
+    contactEmail: string
+    grossAnnualSales: number
+    annualPayroll: number
+    numEmployees: number
+    industryId: string
+    zip: string
+  }>(
+    ({
+      quoteFlow: {
+        businessName,
+        contactEmail,
+        grossAnnualSales,
+        annualPayroll,
+        numEmployees,
+        industryId,
+        zip
+      }
+    }) =>
+      ({
+        businessName,
+        contactEmail,
+        grossAnnualSales,
+        annualPayroll,
+        numEmployees,
+        industryId,
+        zip
+      })
+  )
+
+  React.useEffect(
+    () => {
+      const formData = {
+        businessName,
+        contactEmail,
+        grossAnnualSales,
+        annualPayroll,
+        numEmployees,
+        industryId,
+        locations: [
+          {
+            zip
+          }
+        ]
+      }
+
+      const controller = new AbortController()
+
+      void fetch( // eslint-disable-line no-void
+        'https://api-sandbox.coterieinsurance.com/v1/commercial/applications',
+        {
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `token ${PARTNER_TOKEN}`
+          },
+          body: JSON.stringify(formData),
+          signal: controller.signal
+        }
+      )
+        .then(async (response): Promise<any> => await response.json())
+        .then((response) => {
+          setResponse(response)
+          setIsLoading(false)
+        })
+
+      return () => {
+        // If the component unmounts, abort the fetch request.
+        controller.abort()
+      }
+    },
+    []
+  )
+
+  return (
+    isLoading
+      ? <p>Loading...</p>
+      : <pre>{JSON.stringify(response)}</pre>
+  )
+}
