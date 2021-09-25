caef5356b58abeac7216a9503ba98949a514733d
Joseph R Miles <joe@jrm.dev>
2021-09-24 20:09:05 -0700

    refactor: fix quote flow import path for tests

--- src/feature/quote-flow/component/QuoteFlow.spec.tsx
+++ src/feature/quote-flow/component/QuoteFlow.spec.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
-import QuoteFlow from './QuoteFlow'
+import { QuoteFlow } from './QuoteFlow'
 
 describe.skip('QuoteFlow', () => {
   describe('Business name section', () => {
